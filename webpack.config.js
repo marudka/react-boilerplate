aef9aeafbe906375369a152d20fbfa58291a6d88
Anna Welc <biuro@bluewolf.pl>
2017-09-09 17:15:50 +0200

    Added css, sass loader and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 var path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry: path.resolve(__dirname, "src/index.jsx"),
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        publicPath: "/"
+        filename: "bundle.js",
+        publicPath: "/dist/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                include: path.resolve(__dirname, "src"),
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015", "react"]
+                }
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
     }
 };
